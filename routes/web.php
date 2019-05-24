88b68e7ef402f68239f7c447cd52499b226557d7
vagrant <vagrant@homestead>
2019-05-24 06:36:05 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 
+Route::resource('users', 'UsersController');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
