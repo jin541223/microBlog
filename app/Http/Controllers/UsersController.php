50b6ed2bc3ee75bd50be709f0f181114c6354c65
vagrant <jin541223@163.com>
2019-06-03 15:46:08 +0000

    模型工厂

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
         // 只让未登录的用户访问注册
         $this->middleware('guest', [
@@ -75,5 +75,11 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+    }
+
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
     }
 }
