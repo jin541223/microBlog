301e532e139f53c5b0d0bb9ed84cd265b602b9c5
vagrant <jin541223@163.com>
2019-06-19 22:34:17 +0000

    移除邮件发送from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -107,13 +107,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'zera@example.com';
-        $name = 'zera';
         $to = $user->email;
         $subject = '感谢注册 Micio Blog！请确认您的邮箱。';
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 }
